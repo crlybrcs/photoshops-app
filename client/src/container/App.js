0d5f9d6a3a60688875aec8161bdffafeaa5ea443
Stephen Coley <stephencoley@Stephens-MacBook-Air.local>
2019-12-09 10:20:15 +0100

    OAuth NavBar

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import './App.css';
-import Home from '../components/Home';
-import { Route } from 'react-router-dom';
-import DragAndDropForm from '../components/DragAndDropForm';
-import AddThing from '../components/AddThing';
+import React from "react";
+import "./App.css";
+// import Home from '../components/Home';
+import { Route } from "react-router-dom";
+import DragAndDropForm from "../components/DragAndDropForm";
+import AddThing from "../components/AddThing";
+import Navbar from "../components/Navbar";
+import { Switch } from "react-router-dom";
+// import { NavLink } from "react-bootstrap";
 
 function App() {
-	return (
-		<div className="App">
-			{/* <Home className="class-home" /> */}
-			<Route exact path="/" render={(props) => <AddThing {...props} className="class-home" />} />
-			<Route exact path="/search/:id" render={(props) => <DragAndDropForm {...props} />} />
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* <Home className="class-home" /> */}
+      {/* <Navbar/> */}
+
+      <Navbar />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => <AddThing { ...props } className="class-home" />}
+        />
+        <Route path="/Login" components={ Login } />
+        <Route path="/SignUp" components={ Signup } />
+
+        <Route
+          exact
+          path="/search/:id"
+          render={props => <DragAndDropForm { ...props } />}
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
