b5ee24b74aeb71ee0e646a91902d8fe6fea79f56
Stephen Coley <stephencoley@Stephens-MacBook-Air.local>
2019-12-09 10:54:36 +0100

    Pushing

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -8,11 +8,12 @@ import Navbar from "../components/Navbar";
 import { Switch } from "react-router-dom";
 // import { NavLink } from "react-bootstrap";
 // import Home from "../components/Home";
-import { Route } from "react-router-dom";
-import DragAndDropForm from "../components/DragAndDropForm";
-import AddThing from "../components/AddThing";
-// import GRForms from "../components/GRForms";
+// import DragAndDropForm from "../components/DragAndDropForm";
+// import AddThing from "../components/AddThing";
+import GRForms from "../components/GRForms";
 // import Test from "../components/Test";
+// import Signup from "../components/Signup";
+// import Login from "../components/Login";
 
 function App() {
   return (
@@ -25,24 +26,24 @@ function App() {
         <Route
           exact
           path="/"
-          render={props => <AddThing { ...props } className="class-home" />}
+          render={props => <AddThing {...props} className="class-home" />}
         />
-        <Route path="/Login" components={ Login } />
-        <Route path="/SignUp" components={ Signup } />
+        {/* <Route path="/Login" component={Login} />
+        <Route path="/SignUp" component={Signup} /> */}
 
         <Route
           exact
           path="/search/:id"
-          render={props => <DragAndDropForm { ...props } />}
+          render={props => <DragAndDropForm {...props} />}
         />
-        
-      <Route
-        exact
-        path="/search/:id"
-        render={props => <GRForms {...props} />}
-      />
+
+        <Route
+          exact
+          path="/search/:id"
+          render={props => <GRForms {...props} />}
+        />
       </Switch>
-    
+
       {/* <Route exact path="/search/:id" render={props => <Test {...props} />} /> */}
     </div>
   );
