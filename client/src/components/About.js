15dc0cf3945f1021764944046c6f1cd636f2fb91
crlybrcs <tatyana_@outlook.com>
2019-12-09 18:36:08 +0100

    navbar ui and browse button

---
+++ client/src/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const About = (props) => {
+	return (
+		<div>
+			<h1>About Photophops</h1>
+			<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Sit amet venenatis urna cursus eget. Eros in cursus turpis massa tincidunt. Leo a
+				diam sollicitudin tempor id eu nisl nunc. Mauris vitae ultricies leo integer malesuada. Id eu nisl nunc
+				mi. Hendrerit gravida rutrum quisque non. Ac orci phasellus egestas tellus. Volutpat blandit aliquam
+				etiam erat velit scelerisque in dictum non. Potenti nullam ac tortor vitae purus faucibus. Adipiscing at
+				in tellus integer feugiat scelerisque varius morbi. Viverra maecenas accumsan lacus vel facilisis
+				volutpat est.
+			</article>
+			<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Sit amet venenatis urna cursus eget. Eros in cursus turpis massa tincidunt. Leo a
+				diam sollicitudin tempor id eu nisl nunc. Mauris vitae ultricies leo integer malesuada. Id eu nisl nunc
+				mi. Hendrerit gravida rutrum quisque non. Ac orci phasellus egestas tellus. Volutpat blandit aliquam
+				etiam erat velit scelerisque in dictum non. Potenti nullam ac tortor vitae purus faucibus. Adipiscing at
+				in tellus integer feugiat scelerisque varius morbi. Viverra maecenas accumsan lacus vel facilisis
+				volutpat est.
+			</article>
+		</div>
+	);
+};
+
+export default About;
--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,40 +1,22 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
-// import Home from '../components/Home';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import DragAndDropForm from '../components/DragAndDropForm';
 import AddThing from '../components/AddThing';
 import Navbar from '../components/Navbar';
-import { Switch } from 'react-router-dom';
-// import { NavLink } from "react-bootstrap";
-// import Home from "../components/Home";
-// import DragAndDropForm from "../components/DragAndDropForm";
-// import AddThing from "../components/AddThing";
-import GRForms from '../components/GRForms';
-=======
-import React from "react";
-import "./App.css";
-import { Route, Switch } from "react-router-dom";
-import DragAndDropForm from "../components/DragAndDropForm";
-import AddThing from "../components/AddThing";
-// import Navbar from "../components/Navbar";
 
-// import { NavLink } from "react-bootstrap";
+import { NavLink } from 'react-bootstrap';
 // import Home from "../components/Home";
-import GRForms from "../components/GRForms";
->>>>>>> development
+import GRForms from '../components/GRForms';
 // import Test from "../components/Test";
 // import Signup from "../components/Signup";
 // import Login from "../components/Login";
-
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			{/* <Home className="class-home" /> */}
-			{/* <Navbar/> */}
 
-<<<<<<< HEAD
 			{/* <Navbar /> */}
 			{/* <Switch> */}
 			<Route exact path="/" render={(props) => <AddThing {...props} className="class-home" />} />
@@ -45,30 +27,6 @@ function App() {
 
 			<Route exact path="/search/:id" render={(props) => <GRForms {...props} />} />
 			{/* </Switch> */}
-=======
-      {/* <Navbar /> */}
-      {/* <Switch> */}
-      <Route
-        exact
-        path="/"
-        render={props => <AddThing {...props} className="class-home" />}
-      />
-      {/* <Route path="/Login" component={Login} />
-        <Route path="/SignUp" component={Signup} /> */}
-
-      <Route
-        exact
-        path="/search/:id"
-        render={props => <DragAndDropForm {...props} />}
-      />
-
-      <Route
-        exact
-        path="/search/:id"
-        render={props => <GRForms {...props} />}
-      />
-      {/* </Switch> */}
->>>>>>> development
 
 			{/* <Route exact path="/search/:id" render={props => <Test {...props} />} /> */}
 		</div>
