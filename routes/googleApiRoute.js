be0cd09c60f0b96d969d027b647725cd0fec694c
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-12-10 18:12:07 +0100

    new branch

--- routes/googleApiRoute.js
+++ routes/googleApiRoute.js
@@ -12,19 +12,19 @@ const client = new vision.ImageAnnotatorClient({
 });
 
 router.post("/upload", (req, res) => {
-  console.log("added!", req.body);
+  // console.log("added!", req.body);
   Thing.create({ imageUrl: req.body.imageUrl }).then(response => {
-    console.log("response from .post /upload:", response);
+    // console.log("response from .post /upload:", response);
     res.json(response);
   });
 });
 
 //
 router.get("/search/:id", async (req, res) => {
-  console.log("req.params from googleApiRoute", req.params);
-  console.log("req.params.id from googleApiRoute:", req.params.id);
+  // console.log("req.params from googleApiRoute", req.params);
+  // console.log("req.params.id from googleApiRoute:", req.params.id);
   Thing.findById(req.params.id).then(response => {
-    console.log("response Thing.findById:", response);
+    // console.log("response Thing.findById:", response);
     const { imageUrl } = response;
     let resultLabels;
     let resultWebEntities;
@@ -41,7 +41,7 @@ router.get("/search/:id", async (req, res) => {
         resultLabels = labels.splice(0, 5).map(item => {
           return `${item.description}`;
         });
-        console.log("NEW LABELS:", resultLabels);
+        // console.log("NEW LABELS:", resultLabels);
         return resultLabels;
       }
     }
@@ -57,7 +57,7 @@ router.get("/search/:id", async (req, res) => {
         resultWebEntities = webDetection.webEntities.splice(0, 5).map(item => {
           return `${item.description}`;
         });
-        console.log("NEW WEB ENT: ", resultWebEntities);
+        // console.log("NEW WEB ENT: ", resultWebEntities);
         // console.log(`  Description: ${webEntity.description}`);
         // console.log(`  Score: ${webEntity.score}`);
         // });
@@ -72,7 +72,7 @@ router.get("/search/:id", async (req, res) => {
       const detections = result.fullTextAnnotation;
       resultText = detections.text;
       // resultText.split('\n').join(' ');
-      console.log("NEW TEXT:", resultText);
+      // console.log("NEW TEXT:", resultText);
       // return
       return resultText.split("\n").join(" ");
     }
@@ -83,7 +83,7 @@ router.get("/search/:id", async (req, res) => {
 
     Promise.all([labelsResults, detectWebResults, detectTextResults]).then(
       promised => {
-        console.log("Promise.all -> promised: ", promised);
+        // console.log("Promise.all -> promised: ", promised);
         const labels = promised[0];
         const webR = promised[1];
         const textR = promised[2];
